5b28a8eb0353423998af15be79fe1826f63ff0d5
krishna devaki <twentyonedot@gmail.com>
2022-06-24 00:46:34 +0530

    test commit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,7 +10,7 @@ const About = () => (
       placeat, accusamus aut saepe.
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+      Lorem ipsum olor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
       voluptatibus distinctio recusandae autem esse explicabo molestias officia
       placeat, accusamus aut saepe.
